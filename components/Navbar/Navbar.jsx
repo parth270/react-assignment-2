0234b3ec0e1ca4257d0b3f2318df521932476c34
parth270 <sakhtlaunda2706@gmail.com>
2022-10-22 10:59:48 +0530

    new components

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Navbar.module.css";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+
+const Navbar = () => {
+  return (
+    <> 
+      <div className={classes.container}>
+        <div className={classes["upper-container"]}>
+          <h1>TANN TRIM</h1>
+          <div>
+            <SearchOutlinedIcon/>
+            <PersonOutlineOutlinedIcon/>
+            <BookmarkBorderOutlinedIcon/>
+            <ShoppingBagOutlinedIcon/>
+          </div>
+        </div>
+        <div className={classes["lower-container"]}></div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
